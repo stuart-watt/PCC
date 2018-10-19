59c3026b5ad48c6595dd1148bf886f22abade1cc
Stuart Watt <stuart.watt@research.uwa.edu.au>
2018-10-19 17:14:59 +0800

    Continued work on GUI

--- main.py
+++ main.py
@@ -7,44 +7,60 @@ Created on Mon Oct  1 13:53:46 2018
 
 
 import image_select
-from tkinter import *
+import cv2
+import tkinter as TK
 from tkinter.filedialog import askopenfilename
+from PIL import ImageTk, Image
 
 
-def get_file():
-    return askopenfilename()
+def get_file(display, location):
+    global filename 
+    filename = askopenfilename()
+    # Display the file path
+    display.insert(0, filename)
+    
+    # Display the image
+    image = Image.open(filename)
+    photo = ImageTk.PhotoImage(image)
+    w = TK.Label(location, image=photo)
+    w.pack(side=TK.TOP, expand=True)
+    image.close()
+        
 
-root = Tk()
+
+root = TK.Tk()
 
 root.title('PCC')
 
 # Create the whole window
-f1 = Frame(root, bg='green', relief='ridge', bd=10, height=550, width=600)
-f2 = Frame(f1, bg='white', height=550, width=600)
-f1.pack(side=TOP)
-f2.pack(side=TOP)
+f1 = TK.Frame(root, bg='green', relief='ridge', bd=10, height=500, width=1000)
+f2 = TK.Frame(f1, bg='white', height=500, width=1000)
 
 # Create the toolbar and image window
-toolbar = Frame(f2, bg='black', width=600, height=10)
-image_window = Frame(f2, bg='', width=600, height=540)
-toolbar.pack(side=TOP, in_=f2)
-image_window.pack(side=TOP)
+toolbar = TK.Frame(f2, bg='gray', height=40, width=1000)
+image_window = TK.Frame(f2, bg='', height=460, width=1000)
 
-get_train = Button(toolbar,
+
+for frame in (f1, f2, toolbar, image_window):
+    frame.pack(side=TK.TOP, expand=True)
+    frame.pack_propagate(0)
+
+get_train = TK.Button(toolbar,
                    text='Select training image',
                    font='Helvetica 10 bold',
                    bg='Blue',
-                   command=lambda: get_file())
-get_train.pack(side=LEFT, fill=X, in_=toolbar)
+                   command=lambda: get_file(display, image_window))
 
-train_model = Button(toolbar,
+train_model = TK.Button(toolbar,
                    text='Train model',
                    font='Helvetica 10 bold',
                    bg='red')
-train_model.pack(side=RIGHT, fill=X, in_=toolbar)
 
-display = Entry(root, width=30, font='Helvetica 10 bold')
-display.pack(side=LEFT)
+get_train.pack(side=TK.LEFT, fill=TK.Y)
+train_model.pack(side=TK.RIGHT, fill=TK.Y)
+
+display = TK.Entry(root, font='Helvetica 10 bold')
+display.pack(side=TK.LEFT, fill=TK.X, expand=True)
 
 entries = {}
 
